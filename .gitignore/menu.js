e0080c2e8b3c2fcd718b1bc5907d867449b78431
dinoumoukeloo <56556162+dinoumoukeloo@users.noreply.github.com>
2019-10-27 09:21:02 +0100

    Update menu.js

--- .gitignore/menu.js
+++ .gitignore/menu.js
@@ -1,1 +1,14 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
+var perfix = ".tt"
+
+client.on('message', message => {
+  if (message.content === 'ping' ) {
+    message.reply('pong !')
+  }
+})
+
+
+
+client.login(precess.env.TOKEN)
